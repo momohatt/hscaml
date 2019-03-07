b6577ecf3aacc16f412a123eaab45da3018ff195
momohatt <momohatt10@gmail.com>
2019-03-07 11:52:11 +0900

    fix genConst

--- typing.hs
+++ typing.hs
@@ -11,6 +11,7 @@ genNewTyVar = do
     put $ i + 1
     return $ TVar $ "t_" ++ show i
 
+-- generate constraint
 genConst :: TyEnv -> Expr -> State Int (Ty, Constraint)
 genConst env e =
     case e of
@@ -20,26 +21,26 @@ genConst env e =
                   Just t -> return (t, [])
       ENot e' -> do
           (t, c) <- genConst env e'
-          case t of
-            TBool -> return (TBool, c)
+          return (TBool, (t, TBool) : c)
       ENeg e' -> do
           (t, c) <- genConst env e'
-          case t of
-            TInt -> return (TInt, c)
+          return (TInt, (t, TInt) : c)
       EBinop op e1 e2 -> do
           (t1, c1) <- genConst env e1
           (t2, c2) <- genConst env e2
-          case (t1, t2) of
-            (TBool, TBool)
-              | op `notElem` [BAdd, BSub, BMul, BDiv] -> return (TBool, c1 ++ c2)
-            (TInt, TInt)
-              | op `notElem` [BAnd, BOr] -> return (TInt, c1 ++ c2)
+          -- Is there a better way to write this?
+          case () of
+            _ | op `elem` [BAnd, BOr] ->
+                return (TBool, [(t1, TBool), (t2, TBool)] ++ c1 ++ c2)
+              | op `elem` [BAdd, BSub, BMul, BDiv] ->
+                return (TInt, [(t1, TInt), (t2, TInt)] ++ c1 ++ c2)
+              | otherwise ->
+                return (TBool, (t1, t2) : c1 ++ c2)
       EIf e1 e2 e3 -> do
           (t1, c1) <- genConst env e1
           (t2, c2) <- genConst env e2
           (t3, c3) <- genConst env e3
-          case t1 of
-            TBool -> return (t2, (t2, t3) : c1 ++ c2 ++ c3)
+          return (t2, [(t1, TBool), (t2, t3)] ++ c1 ++ c2 ++ c3)
       ELet x e1 e2 -> do
           (t1, c1) <- genConst env e1
           (t2, c2) <- genConst ((x, t1) : env) e2
@@ -68,11 +69,11 @@ unify c =
             (TBool, TBool) -> unify xc
 
 typeCheck :: Command -> IO ()
-typeCheck c =
-    case c of
-      CExpr e -> do
-          let (_, const) = evalState (genConst [] e) 0
-          case unify const of
-            Just _ -> return ()
-            Nothing ->
-                print "unify error"
+typeCheck c = do
+    let body = case c of
+                 CExpr e -> e
+                 CDecl (DLet _ e) -> e
+                 CDecl (DLetRec _ _ e) -> e
+    let (_, const) = evalState (genConst [] body) 0
+    case unify const of
+      Just _ -> return ()
