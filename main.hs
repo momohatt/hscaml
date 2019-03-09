d5df7bca90f89acd567a3cb07e4cfd7b6e3670c0
momohatt <momohatt10@gmail.com>
2019-03-09 15:44:25 +0900

    use haskeline

--- main.hs
+++ main.hs
@@ -1,7 +1,8 @@
 module Main where
 
-import System.IO
 import Control.Exception
+import System.Console.Haskeline
+import System.IO
 
 import Syntax
 import Parser
@@ -12,33 +13,33 @@ import Typing
 initEnv = []
 initTenv = []
 
-repl :: Int -> TyEnv -> Env -> IO ()
+repl :: Int -> TyEnv -> Env -> InputT IO ()
 repl n tenv env = do
-    putStr "# " >> hFlush stdout
-    input <- getLine
-    if input == "quit"
-        then
-            return ()
-        else do
-            let parsedProg = parseString input
-            -- print parsedProg
-            case typeCheck n tenv parsedProg of
-              Left msg -> do
-                  putStrLn ("TypeError: " ++ msg)
-                  repl n tenv env
-              Right (t, tenv', c, n) -> do
-                -- print c -- for debug
-                -- print tenv'
-                -- print t
-                case parsedProg of
-                  CExpr e -> do
-                      putStrLn $ "- : " ++ tyToStr t ++ " = " ++ valToStr (eval env e)
-                      repl n tenv' env
-                  CDecl e -> do
-                      let (env', v) = evalDecl env e
-                      putStrLn $ "val " ++ nameOfDecl e ++ " : " ++ tyToStr t ++ " = " ++ valToStr v
-                      repl n tenv' env'
+    minput <- getInputLine "# "
+    case minput of
+      Nothing -> return ()
+      Just "quit" -> return ()
+      Just "exit" -> return ()
+      Just input -> do
+          let parsedProg = parseString input
+          -- print parsedProg
+          case typeCheck n tenv parsedProg of
+            Left msg -> do
+                outputStrLn ("TypeError: " ++ msg)
+                repl n tenv env
+            Right (t, tenv', c, n) -> do
+              -- print c -- for debug
+              -- print tenv'
+              -- print t
+              case parsedProg of
+                CExpr e -> do
+                    outputStrLn $ "- : " ++ tyToStr t ++ " = " ++ valToStr (eval env e)
+                    repl n tenv' env
+                CDecl e -> do
+                    let (env', v) = evalDecl env e
+                    outputStrLn $ "val " ++ nameOfDecl e ++ " : " ++ tyToStr t ++ " = " ++ valToStr v
+                    repl n tenv' env'
 
 main :: IO ()
 main =
-    repl 0 initTenv initEnv
+    runInputT defaultSettings $ repl 0 initTenv initEnv
