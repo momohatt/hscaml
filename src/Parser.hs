e513ee9a04cef10e9343ffaee4570ff13cc1e67b
momohatt <momohatt10@gmail.com>
2019-12-07 21:59:10 +0900

    Implement new parser except for patterns

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,7 +3,9 @@ module Parser
   ) where
 
 import           Control.Applicative        (pure, (*>), (<$>), (<$), (<*), (<*>))
+import           Control.Monad.Combinators.Expr
 import           Data.Functor               (($>))
+import           Data.Maybe                 (isJust)
 import           Data.Void
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
@@ -15,37 +17,95 @@ type Parser = Parsec Void String
 
 parseCmd :: String -> Either String Command
 parseCmd input =
-  case parse command "" input of
+  case parse (sc >> command <* symbol ";;") "" input of
     Left err -> Left (errorBundlePretty err)
     Right c  -> Right c
 
 command :: Parser Command
-command = CDecl <$> (reserved "let" >> decl)
+command = toplevelLet
       <|> CExpr <$> expr
       <?> "command"
 
+-- Parser after "let"
 decl :: Parser Decl
-decl = DLetRec <$> (try (reserved "rec") >> lowerId) <*> expr
-   <|> DLet <$> lowerId <*> expr
-   <?> "let declaration"
+decl = do
+  isRec <- isJust <$> optional (reserved "rec")
+  var   <- lowerId
+  args  <- many lowerId
+  body  <- symbol "=" >> expr
+  return $ if isRec then DLetRec var (foldr EFun body args)
+                    else DLet    var (foldr EFun body args)
+
+toplevelLet :: Parser Command
+toplevelLet = do
+  bind <- reserved "let" >> decl
+  body <- optional (reserved "in" >> expr)
+  case body of
+    Nothing -> return $ CDecl bind
+    Just b' -> return $ CExpr (ELetIn bind b')
 
 expr :: Parser Expr
-expr = atomExpr
+expr = EIf    <$> (reserved "if" >> expr) <*> (reserved "then" >> expr)
+                                          <*> (reserved "else" >> expr)
+   <|> ELetIn <$> (reserved "let" >> decl)    <*> (reserved "in" >> expr)
+   <|> EFun   <$> (reserved "fun" >> lowerId) <*> (symbol "->" >> expr)
+   <|> EMatch <$> (reserved "match" >> expr)  <*> (reserved "with" >> matchClause1)
+   <|> opExpr
 
-tupleOrParenExpr :: Parser Expr
-tupleOrParenExpr = do
+matchClause1 :: Parser [(Pattern, Expr)]
+matchClause1 = do
+  first <- optional (symbol "|") >> matchClause
+  rest  <- many (symbol "|" >> matchClause)
+  return (first : rest)
+  where
+    matchClause :: Parser (Pattern, Expr)
+    matchClause = (,) <$> pattern <*> expr
+
+opExpr :: Parser Expr
+opExpr = makeExprParser atomExpr table
+  where
+    table :: [[Operator Parser Expr]]
+    table =
+      [ [ InfixL (EApp        <$ sc         ) ]
+      , [ Prefix (ENeg        <$ symbol "-" ) ]
+      , [ InfixL (EBinop BMul <$ symbol "*" )
+        , InfixL (EBinop BDiv <$ symbol "/" ) ]
+      , [ InfixL (EBinop BAdd <$ symbol "+" )
+        , InfixL (EBinop BSub <$ symbol "-" ) ]
+      , [ InfixR (ECons       <$ symbol "::") ]
+      , [ InfixL (EBinop BEq  <$ symbol "=" ) ]
+      , [ InfixL (EBinop BGE  <$ symbol ">=")
+        , InfixL (EBinop BLE  <$ symbol "<=")
+        , InfixL (EBinop BGT  <$ symbol ">" )
+        , InfixL (EBinop BLT  <$ symbol "<" ) ]
+      , [ InfixL (EBinop BAnd <$ symbol "&&") ]
+      , [ InfixL (EBinop BOr  <$ symbol "||") ]
+      ]
+
+parenExpr :: Parser Expr
+parenExpr = do
   elems <- parens $ sepBy expr comma
   case elems of
     [x] -> return x
     _   -> return $ ETuple elems
 
+bracketExpr :: Parser Expr
+bracketExpr = do
+  elems <- brackets $ sepBy expr (symbol ";")
+  case elems of
+    [] -> return ENil
+    _  -> return $ foldr ECons ENil elems
+
 atomExpr :: Parser Expr
 atomExpr = EConstInt  <$> positiveIntegerLiteral
        <|> EConstBool <$> boolLiteral
        <|> EVar       <$> lowerId
-       <|> tupleOrParenExpr
-       <|> ENil       <$  brackets sc
+       <|> parenExpr
+       <|> bracketExpr
        <?> "atomic expression"
+
+pattern :: Parser Pattern
+pattern = undefined
 
 --
 -- Tokens
@@ -66,8 +126,8 @@ positiveIntegerLiteral = lexeme L.decimal
                      <?> "unsinged integer"
 
 boolLiteral :: Parser Bool
-boolLiteral = reserved "True"  $> True
-          <|> reserved "False" $> False
+boolLiteral = reserved "true"  $> True
+          <|> reserved "false" $> False
           <?> "boolean"
 
 parens :: Parser a -> Parser a
